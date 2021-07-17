5c8b664d94831e664d540dd796107b539e932333
yonii <qkrtndus130@gmail.com>
2021-07-17 23:33:24 +0900

    Update 격자판최대합.java
    code 최적화 & 고려안한 부분 추가

--- 자바 알고리즘 강의/격자판최대합.java
+++ 자바 알고리즘 강의/격자판최대합.java
@@ -3,41 +3,40 @@ package Inflearn;
 import java.util.Scanner;
 
 public class 격자판최대합 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[][] array = new int[n][n];
+   public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int[][] array = new int[n][n];
+            int answer = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                array[i][j] = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    array[i][j] = in.nextInt();
+                }
             }
-        }
+            
+            int total1 = 0;
+            int total2 = 0;
+            //행, 열
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    total1 += array[i][j];
+                    total2 += array[j][i];
+                }
+                answer = Math.max(answer, total1);
+                answer = Math.max(answer,total2);
+                total1 = 0;
+                total2 = 0;
+            }
 
-        int max = -1;
-        int total = 0;
-        //행
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                total += array[i][j];
+            //대각선
+            for (int i = 0; i < n; i++) {
+                total1 += array[i][i];
+                total2 += array[i][n-i-1];
             }
-            max = Math.max(max, total);
-            total = 0;
-        }
-        //열
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                total += array[j][i];
-            }
-            max = Math.max(max, total);
-            total = 0;
-        }
-        //대각선
-        for (int i = 0; i < n; i++) {
-            total += array[i][i];
-        }
-        max = Math.max(max, total);
+            answer = Math.max(answer, total1);
+            answer = Math.max(answer,total2);
 
-        System.out.println(max);
-    }
+            System.out.println(answer);
+        }
 }
