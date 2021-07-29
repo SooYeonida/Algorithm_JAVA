6499a4b4e80f59f93a0c045ef4cf4bdefe8922c1
sooyeon <qkrtndus130@gmail.com>
2021-07-29 20:16:57 +0900

    stack,queue 7

---
+++ Java Lecture Problems/Stack,Queue/교육과정설계.java
@@ -0,0 +1,45 @@
+package Inflearn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 교육과정설계 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String must = in.next();
+        String plan = in.next();
+
+        ArrayList<Character> arrayList = new ArrayList<>();
+        for (char c : must.toCharArray()) {
+            arrayList.add(c);
+        }
+
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : plan.toCharArray()) {
+            queue.add(c);
+        }
+
+        boolean answer = false;
+        int index = 0;
+        while (!queue.isEmpty() && index < must.length()) {
+            if (queue.poll() == must.charAt(index)) {
+                index++;
+                answer = true;
+            }
+        }
+
+        if(answer) {
+            if (index != must.length()) {
+                System.out.println("NO");
+            }
+            else if(index == must.length()){
+                System.out.println("YES");
+            }
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
