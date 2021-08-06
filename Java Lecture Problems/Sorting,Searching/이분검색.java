d44a1a91efa31a6590f32620a58aa31845f3a578
yonii <qkrtndus130@gmail.com>
2021-08-06 13:41:54 +0900

    Update 이분검색.java
    빠진 코드 추가

--- Java Lecture Problems/Sorting,Searching/이분검색.java
+++ Java Lecture Problems/Sorting,Searching/이분검색.java
@@ -8,15 +8,39 @@ public class 이분검색 {
         int n = in.nextInt(); //숫자 개수
         int m = in.nextInt(); //찾을 숫자
 
-        ArrayList<Integer> list = new ArrayList<>();
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for(int i=0;i<n;i++){
+//            list.add(in.nextInt());
+//        }
+//        Collections.sort(list);
+//
+//        //12 23 32 57 65 81 87 99
+//
+//        System.out.println(list.indexOf(m)+1);
+// arraylist로 단순 구현        
+        
+
+        int[] arr = new int[n];
         for(int i=0;i<n;i++){
-            list.add(in.nextInt());
+            arr[i] = in.nextInt();
         }
-        Collections.sort(list);
 
-        //12 23 32 57 65 81 87 99
+        Arrays.sort(arr);
+        
+        int lt = 0, rt = n-1;
+        int answer = -1;
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(arr[mid]==m){
+                answer = mid+1;
+                break;
+            }
+            if(arr[mid]>m) rt= mid-1;
+            else lt = mid+1;
+        }
 
-        System.out.println(list.indexOf(m)+1);
-
+        System.out.println(answer);
+        
+        //이분 검색 구현
     }
 }
