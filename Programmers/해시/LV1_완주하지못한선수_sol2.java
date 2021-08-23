2247075dfe4ae22d302178df33774403bd5bb314
sooyeon <qkrtndus130@gmail.com>
2021-08-23 23:15:01 +0900

    완주하지 못한 선수
    해시 복습차원 2번째 풀이

---
+++ Programmers/해시/LV1_완주하지못한선수_sol2.java
@@ -0,0 +1,27 @@
+package Problem.해시;
+import java.util.*;
+public class LV1_완주하지못한선수_sol2 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String c : completion){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(String p : participant){
+            if(!map.containsKey(p)){
+                answer = p;
+                break;
+            }
+            else{
+                if(map.get(p)<=0){
+                    answer = p;
+                }
+                else{
+                    map.put(p,map.get(p)-1);
+                }
+            }
+        }
+        return answer;
+    }
+}
