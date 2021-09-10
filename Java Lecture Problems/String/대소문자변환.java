f38d19ec7b4cbc4430eb6b1b3b30e6948631a3c2
yonii <qkrtndus130@gmail.com>
2021-09-11 02:30:00 +0900

    대소문자 변환
    String

--- Java Lecture Problems/String/대소문자변환.java
+++ Java Lecture Problems/String/대소문자변환.java
@@ -1,23 +1,19 @@
-package Inflearn;
 import java.util.Scanner;
-
+  
 public class 대소문자변환 {
-
-        public static void main(String[] args){
-            Scanner in=new Scanner(System.in);
-            String str = in.next();
-
-            String answer = "";
-
-            for(char x : str.toCharArray()){
-                if(Character.isUpperCase(x)){
-                    answer += Character.toLowerCase(x);
-                }
-                else{
-                    answer += Character.toUpperCase(x);
-                }
-            }
-            System.out.println(answer);
-            return ;
-        }
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+ 	String str = in.next();
+    String answer = "";
+	for(char c : str.toCharArray()){
+      if(Character.isUpperCase(c)){
+        answer += String.valueOf(c).toLowerCase();
+      }
+      else{
+        answer += String.valueOf(c).toUpperCase();
+      }
+    }
+    System.out.println(answer);
+    return ;
+  }
 }
