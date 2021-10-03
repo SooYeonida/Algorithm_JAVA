0802c5a780103496373a30210f35905ab04849d9
yonii <qkrtndus130@gmail.com>
2021-10-03 23:51:24 +0900

    뒤집은 소수
    Array, 복습

--- Java Lecture Problems/Array/뒤집은소수.java
+++ Java Lecture Problems/Array/뒤집은소수.java
@@ -3,33 +3,24 @@ package Inflearn;
 import java.util.Scanner;
 
 public class 뒤집은소수 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String[] array = new String[n];
+   public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+       int[] arr = new int[n];
 
-        for(int i=0;i<n;i++){
-            array[i] = in.next();
-        }
+       for(int i=0;i<n;i++){
+           String str = new StringBuilder(in.next()).reverse().toString();
+           arr[i] = Integer.parseInt(str);
+       }
+       
 
-        for(int i=0;i<n;i++){
-            StringBuilder sb = new StringBuilder(array[i]);
-            int num = Integer.parseInt(sb.reverse().toString());
-
-            if(isPrime(num)){
-                System.out.printf("%d ",num);
-            }
-            //while(tmp>0){
-            //int t = tmp%10;
-            //res =res*10 + t;
-            //tmp = tmp/10;
-            //라이브러리 못쓸때는 이런식으로 뒤집기
-
-        }
+       for(int a : arr){
+           if(isPrime(a)) System.out.print(a+" ");
+       }
     }
-
-    public static boolean isPrime(int num){
-        if(num ==1) return false;
+    
+    static boolean isPrime(int num){
+        if(num == 1) return false;
         else{
             for(int i=2;i<num;i++){
                 if(num%i==0){
