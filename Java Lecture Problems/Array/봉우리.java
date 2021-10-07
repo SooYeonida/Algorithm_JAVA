eaecf93ff7cffe40501e04ded012ea56a122fc8a
yonii <qkrtndus130@gmail.com>
2021-10-07 13:08:30 +0900

    봉우리
    Array

--- Java Lecture Problems/Array/봉우리.java
+++ Java Lecture Problems/Array/봉우리.java
@@ -6,32 +6,34 @@ public class 봉우리 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[][] array = new int[n + 2][n + 2];
+        int[][] board = new int[n+2][n+2];
 
-        for (int i = 0; i < n + 2; i++) {
-            for (int j = 0; j < n + 2; j++) {
-                if (i != 0 && j != 0 && i != n+1 && j != n+1) {
-                    array[i][j] = in.nextInt();
-                } else {
-                    array[i][j] = 0;
-                }
+        //상하좌우
+        int[] dx = {0,0,-1,1};
+        int[] dy = {1,-1,0,0};
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                board[i][j] = in.nextInt();
             }
         }
 
-        int total = 0;
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < n+1; j++) {
-                int center = array[i][j];
-                int up = array[i - 1][j];
-                int down = array[i + 1][j];
-                int left = array[i][j - 1];
-                int right = array[i][j + 1];
-                if (center > up && center > down && center > left && center > right) {
-                    total++;
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                //상하좌우 검사
+                boolean check = true;
+                for(int k=0;k<4;k++) {
+                    if(board[i+dx[k]][j+dy[k]]>=board[i][j]){
+                        check = false;
+                        break;
+                    }
                 }
+                if(check) count++;
+
             }
         }
 
-        System.out.println(total);
+        System.out.println(count);
     }
 }
