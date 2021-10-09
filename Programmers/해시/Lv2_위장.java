9f08b0a29f6aaac6e09f1a66e4d7bee92346767c
yonii <qkrtndus130@gmail.com>
2021-10-09 17:07:35 +0900

    위장
    Hash, 더 간결하게 구현

--- Programmers/해시/Lv2_위장.java
+++ Programmers/해시/Lv2_위장.java
@@ -2,28 +2,19 @@ package Problem.해시;
 import java.util.*;
 
 public class Lv2_위장 {
-
-        public int solution(String[][] clothes) {
-            int answer = 1;
-            HashMap<String,Integer> hashMap = new HashMap<>();
-
-            for(String[]clothe : clothes){
-                //초기값 설정
-                if(!hashMap.keySet().contains(clothe[1])){
-                    hashMap.put(clothe[1],1);
-                }
-                else{
-                    //값 증가
-                    int entry = hashMap.get(clothe[1]);
-                    hashMap.put(clothe[1],++entry);
-                }
-            }
-
-            for(String key : hashMap.keySet()){
-                answer *= (hashMap.get(key) + 1);
-            }
-            answer --;
-
-            return answer;
+       public int solution(String[][] clothes) {
+        HashMap<String,Integer> map = new HashMap<>();
+        
+         for(int i=0;i<clothes.length;i++){
+             map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
         }
+        
+        //경우의 수 
+        int answer = 1;
+        for(String key : map.keySet()){
+            System.out.println(map.get(key));
+            answer *= (map.get(key)+1); //+1 : 아예 안입는 경우 
+        }
+        return answer-1; //모든 타입의 의상을 안입는 경우 -1 
+    }
 }
